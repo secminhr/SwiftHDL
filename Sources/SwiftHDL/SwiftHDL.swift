0008468ec3256a412a8dfe046166fd26df5eef00
secminhr <secminhrian@gmail.com>
2021-05-02 13:35:51 +0800

    init commit

--- Sources/SwiftHDL/SwiftHDL.swift
+++ Sources/SwiftHDL/SwiftHDL.swift
@@ -1,3 +1,180 @@
-struct SwiftHDL {
-    var text = "Hello, World!"
+import Combine
+import Foundation
+
+public class Chip {
+    var cancellables: [AnyCancellable] = []
+    public init() {
+        create()
+    }
+    func create() {}
+    
+    func connections(@ConnectionBuilder _ connect: () -> [AnyCancellable]) {
+        cancellables = connect()
+    }
+}
+
+public class And: Chip {
+    @Input public var inA: Bit
+    @Input public var inB: Bit
+    @Output public var out: Bit
+    
+    public override init() {
+        super.init()
+    }
+    
+    var nand = Nand()
+    let not = Not()
+    
+    override func create() {
+//        nand = Nand($inA, $inB)
+        connections {
+            $inA ~> nand.$inA
+            $inB ~> nand.$inB
+            nand.$out ~> not.$input
+            not.$output ~> $out
+//            $inA.send(nand.$inA, onSend: { print("And.inA send to nand.inA") })
+//            $inB.send(nand.$inB, onSend: { print("And.inB send to nand.inB") })
+//            nand.$out.send(not.$input, onSend: { print("nand.out send to not.input: \($0)") })
+//            not.$output.send($out, onSend: { print("not.output send to And.out: \($0)") })
+        }
+    }
+}
+
+public class Not: Chip {
+    @Input public var input: Bit
+    @Output public var output: Bit
+
+    public override init() {
+        super.init()
+    }
+    
+    let nand = Nand()
+    override func create() {
+        connections {
+            $input ~> nand.$inA
+            $input ~> nand.$inB
+            nand.$out ~> $output
+        }
+    }
+}
+
+
+public class Nand: Chip {
+    @Input public var inA: Bit
+    @Input public var inB: Bit
+    @Output public var out: Bit
+    
+    private var cancellable: [AnyCancellable] = []
+    
+    public init(_ inA: Input, _ inB: Input) {
+        super.init()
+        cancellable += [
+            inA.send(self.$inA, onSend: { print("construct inA send to Nand.inA: \($0)") }),
+            inB.send(self.$inB, onSend: { print("construct inB send to Nand.inB: \($0)") })
+        ]
+        cancellable.append(connection())
+    }
+    
+    override public init() {
+        super.init()
+        cancellable.append(connection())
+    }
+    
+    private func connection() -> AnyCancellable {
+        Publishers.CombineLatest($inA.subject, $inB.subject)
+            .map { (a, b) -> Bit in
+                return a == .one && b == .one ? .zero : .one
+            }.sink {
+                self.out = $0
+            }
+    }
+}
+
+public enum Bit: Int {
+    case zero = 0
+    case one = 1
+}
+
+
+
+
+@propertyWrapper
+public class Input {
+    fileprivate let subject: PassthroughSubject<Bit, Never>
+    
+    public init() {
+        subject = PassthroughSubject()
+    }
+    
+    public var wrappedValue: Bit {
+        set { subject.send(newValue) }
+        get { fatalError("Should not get from input.") }
+    }
+    
+    public var projectedValue: Input {
+        return self
+    }
+    
+    static func ~> (left: Input, right: Input) -> AnyCancellable {
+        left.subject.sink(receiveValue: right.subject.send)
+    }
+    
+    
+    func send(_ right: Input, onSend: @escaping (Bit) -> Void) -> AnyCancellable {
+        return subject.sink {
+            onSend($0)
+            right.subject.send($0)
+        }
+    }
+}
+
+@propertyWrapper
+public class Output {
+    fileprivate let subject: CurrentValueSubject<Bit, Never>
+    
+    public convenience init () {
+        self.init(wrappedValue: Bit.zero)
+    }
+    
+    public init(wrappedValue: Bit) {
+        subject = CurrentValueSubject(wrappedValue)
+    }
+    
+    public var wrappedValue: Bit {
+        set { subject.send(newValue) }
+        get { subject.value }
+    }
+    
+    public var projectedValue: Output {
+        return self
+    }
+    
+    static func ~> (left: Output, right: Output) -> AnyCancellable {
+        left.subject.sink(receiveValue: right.subject.send)
+    }
+    
+    static func ~> (left: Output, right: Input) -> AnyCancellable {
+        left.subject.sink(receiveValue: right.subject.send)
+    }
+    
+    func send(_ right: Output, onSend: @escaping (Bit) -> Void) -> AnyCancellable {
+        return subject.sink {
+            onSend($0)
+            right.subject.send($0)
+        }
+    }
+    
+    func send(_ right: Input, onSend: @escaping (Bit) -> Void) -> AnyCancellable {
+        return subject.sink {
+            onSend($0)
+            right.subject.send($0)
+        }
+    }
+}
+
+@_functionBuilder
+struct ConnectionBuilder {
+    static func buildBlock(_ cancellables: AnyCancellable...) -> [AnyCancellable] {
+        return cancellables
+    }
 }
--- Tests/SwiftHDLTests/SwiftHDLTests.swift
+++ Tests/SwiftHDLTests/SwiftHDLTests.swift
@@ -1,15 +1,48 @@
 import XCTest
-@testable import SwiftHDL
+import SwiftHDL
 
 final class SwiftHDLTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftHDL().text, "Hello, World!")
+    
+    func testAnd() {
+        let and = And()
+
+        and.inA = .zero
+        and.inB = .zero
+        assert(and.out == .zero)
+
+        and.inB = .one
+        assert(and.out == .zero)
+
+        and.inA = .one
+        and.inB = .zero
+        assert(and.out == .zero)
+
+        and.inB = .one
+        assert(and.out == .one)
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testNot() {
+        let not = Not()
+        not.input = .zero
+        assert(not.output == .one)
+
+        not.input = .one
+        assert(not.output == .zero)
+    }
+
+    func testNand() {
+        let nand = Nand()
+        nand.inA = .zero
+        nand.inB = .zero
+        assert(nand.out == .one)
+
+        nand.inB = .one
+        assert(nand.out == .one)
+
+        nand.inA = .one
+        assert(nand.out == .zero)
+
+        nand.inB = .zero
+        assert(nand.out == .one)
+    }
 }
